28f79c1bb51cfb5af1a58a52ddfedc9ba3e2cf59
yisus418 <dev.jcenobio@gmail.com>
2024-12-02 00:04:17 -0600

    initial game view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { HangImage } from "./components/HangImage";
+import { letters } from "./helpers/letters";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <h1>¡Hola mundo!</h1>
+        {/* Imagen del juego */}
+        <HangImage imageNumber={ 0 } />
+
+        {/* Palabra oculta */}
+        <h3 className="text-center">_ _ _ _ _ _ _ _ _ _ _ _</h3>
+
+        {/* Contador intentos */}
+        <h3 className="text-center">Intentos: 0</h3>
+
+        {/* Botones de letras */}
+        <div className="flex justify-center items-center">
+          {
+            letters.map((letter) => (
+              <button key={letter}>{letter}</button>
+            ))
+          }
+        </div>
       </div>
     </>
   );

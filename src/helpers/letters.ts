7e2f92ba6037b6ba3e77aec703f2c6a0bba91b60
yisus418 <dev.jcenobio@gmail.com>
2024-12-01 21:49:57 -0600

    Relocate folder helpers to src/ directory

--- src/helpers/letters.ts
+++ src/helpers/letters.ts
@@ -1,4 +1,4 @@
-export const letters = [
+export const letters: string[] = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'

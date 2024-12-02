10b01ec0c3040ef3a9c1b27273c15b06a9358bae
yisus418 <dev.jcenobio@gmail.com>
2024-12-02 00:06:49 -0600

    add failed attempts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
+import { useState } from "react";
 import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
 import "./App.css";
 
 function App() {
+  const [attempts, setAttempts] = useState(0);
+
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+
+    setAttempts(attempts + 1);
+  }
+
   return (
     <>
       <div>
         {/* Imagen del juego */}
-        <HangImage imageNumber={ 0 } />
+        <HangImage imageNumber={ attempts } />
 
         {/* Palabra oculta */}
         <h3 className="text-center">_ _ _ _ _ _ _ _ _ _ _ _</h3>
 
         {/* Contador intentos */}
-        <h3 className="text-center">Intentos: 0</h3>
+        <h3 className="text-center">Intentos: { attempts }</h3>
 
         {/* Botones de letras */}
         <div className="flex justify-center items-center">
           {
             letters.map((letter) => (
-              <button key={letter}>{letter}</button>
+              <button onClick={ () => checkLetter(letter) } key={letter}>
+                {letter}
+              </button>
             ))
           }
         </div>
